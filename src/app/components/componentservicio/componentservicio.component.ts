5501ff96c9d631a5b314a71daa19d9feed41e4d8
elkinrodriguez <erodriguezf@uninorte.edu.co>
2021-11-05 17:04:46 -0500

    sixth commit

--- src/app/components/componentservicio/componentservicio.component.ts
+++ src/app/components/componentservicio/componentservicio.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {HttpClientModule} from '@angular/common/http';
-//import { Console } from 'console';
+
 
 
 @Component({
